0876a0df39ffda2a97e9d6a50fd051d3bb2c61e2
Brandon Carl <bcarl@me.com>
2012-11-21 16:19:45 -0800

    Refactor execute path and add debug logging.

--- src/main/java/edu/ucsd/crbs/confluence/plugins/latex/CachedLaTeXMacro.java
+++ src/main/java/edu/ucsd/crbs/confluence/plugins/latex/CachedLaTeXMacro.java
@@ -1,13 +1,14 @@
 package edu.ucsd.crbs.confluence.plugins.latex;
 
-import java.util.Map;
-
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.content.render.xhtml.DefaultConversionContext;
 import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
 import com.atlassian.confluence.pages.Attachment;
 import com.atlassian.confluence.pages.AttachmentManager;
+import com.atlassian.confluence.pages.Comment;
+import com.atlassian.confluence.pages.PageManager;
 import com.atlassian.confluence.renderer.PageContext;
 import com.atlassian.confluence.setup.settings.SettingsManager;
 import com.atlassian.renderer.RenderContext;
@@ -23,17 +24,22 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang.StringUtils;
+
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CachedLaTeXMacro extends BaseMacro implements Macro
 {
@@ -53,43 +59,97 @@ public class CachedLaTeXMacro extends BaseMacro implements Macro
 	private static final String DOT = ".";
 	private static final String ATTACHMENT_EXT = "png";
 	private static final int ATTACHMENT_COMMENT_MAX_LENGTH = 254;
+	private static final String ATTACHMENT_COMMENT_SUFFIX = "...";
+	private static final String ATTACHMENT_MIMETYPE = "image/png";
 
 	private final AttachmentManager attachmentManager;
 	private final SettingsManager settingsManager;
+	private final PageManager pageManager;
 
-	public CachedLaTeXMacro(AttachmentManager attachmentManager, SettingsManager settingsManager)
+	private static final Logger log = LoggerFactory.getLogger(CachedLaTeXMacro.class);
+
+	public CachedLaTeXMacro(AttachmentManager attachmentManager, SettingsManager settingsManager, PageManager pageManager)
 	{
 		this.attachmentManager = attachmentManager;
 		this.settingsManager = settingsManager;
+		this.pageManager = pageManager;
 	}
 
 	// Confluence < 4.0
+	// @SuppressWarnings({ "unchecked", "rawtypes" })
+	// @Override
+	// public String execute(Map parameters, String body, RenderContext context) throws MacroException
+	// {
+	// 	if (context.getClass().equals(PageContext.class)) {
+	// 		try {
+	// 			return execute(parameters, body, ((PageContext) context).getEntity());
+	// 		} catch (MacroExecutionException e) {
+	// 			throw new MacroException(e);
+	// 		}
+	// 	}
+	// 	throw new MacroException("LaTeX macro is only available on Pages.");
+	// }
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public String execute(Map parameters, String body, RenderContext context) throws MacroException
+	public String execute(Map parameters, String body, RenderContext renderContext) throws MacroException
 	{
-		if (context.getClass().equals(PageContext.class)) {
-			try {
-				return execute(parameters, body, ((PageContext) context).getEntity());
-			} catch (MacroExecutionException e) {
-				throw new MacroException(e);
-			}
+		try
+		{
+			return execute(parameters, body, new DefaultConversionContext(renderContext));
 		}
-		throw new MacroException("LaTeX macro is only available on Pages.");
+		catch (MacroExecutionException e)
+		{
+			throw new MacroException(e);
+		}
 	}
 
 	// Confluence 4.0 +
-	@Override
-	public String execute(Map<String, String> parameters, String body, ConversionContext context) throws MacroExecutionException
-	{
-		return execute(parameters, body, context.getEntity());
-	}
+	// @Override
+	// public String execute(Map<String, String> parameters, String body, ConversionContext context) throws MacroExecutionException
+	// {
+	// 	return execute(parameters, body, context.getEntity());
+	// }
 
 	/**
-	 * This method returns XHTML to be displayed on the page that uses this macro.
+	 * Process a request to render an {attachments} macro
 	 */
-	private String execute(Map<String, String> parameters, String body, ContentEntityObject page) throws MacroExecutionException
+	@Override
+	public String execute(Map<String, String> parameters, String body, ConversionContext conversionContext) throws MacroExecutionException
 	{
+		String pageTitle = parameters.get("page");
+		ContentEntityObject contentObject;
+		PageContext pageContext;
+
+		log.debug("Page Title: {}", pageTitle);
+
+		if (StringUtils.isNotBlank(pageTitle))
+		{
+			contentObject = getPage(conversionContext.getPageContext(), pageTitle);
+			pageContext = new PageContext(contentObject);
+			if (contentObject == null)
+			{
+				return null;
+			}
+		}
+		else
+		{
+			// retrieve a reference to the body object this macro is in
+			contentObject = conversionContext.getEntity();
+			pageContext = conversionContext.getPageContext();
+		}
+		String sortBy = null;
+		String sortOrder = null;
+
+		// CONF-9989: if this macro is run from within a comment, use the underlying page to find the attachments
+		if (contentObject instanceof Comment)
+			contentObject = ((Comment) contentObject).getOwner();
+
+	// /**
+	//  * This method returns XHTML to be displayed on the page that uses this macro.
+	//  */
+	// private String execute(Map<String, String> parameters, String body, ContentEntityObject page) throws MacroExecutionException
+	// {
 		body = body.trim();
 
 		if (body.length() < 1)
@@ -100,10 +160,14 @@ public class CachedLaTeXMacro extends BaseMacro implements Macro
 		String latexHash = SHA1(body);
 		String attachmentFileName = latexHash + DOT + ATTACHMENT_EXT;
 
-		Attachment attachment;
+		log.debug("Attachment Filename: {}", attachmentFileName);
 
-		if (null == (attachment = this.attachmentManager.getAttachment(page, attachmentFileName)))
+		Attachment attachment = attachmentManager.getAttachment(contentObject, attachmentFileName);
+
+		if (attachment == null)
 		{
+			log.debug("Attachment was null, need to create new. contentObject: {}", contentObject.toString());
+
 			// need to generate image
 			TeXFormula formula = new TeXFormula(body);
 			TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 20);
@@ -134,23 +198,47 @@ public class CachedLaTeXMacro extends BaseMacro implements Macro
 
 			String attachmentComment = body;
 			if (attachmentComment.length() > ATTACHMENT_COMMENT_MAX_LENGTH) {
-				attachmentComment = attachmentComment.substring(0, ATTACHMENT_COMMENT_MAX_LENGTH);
+				attachmentComment = attachmentComment.substring(0, ATTACHMENT_COMMENT_MAX_LENGTH - ATTACHMENT_COMMENT_SUFFIX.length()) + ATTACHMENT_COMMENT_SUFFIX;
 			}
 
-			attachment = new Attachment(attachmentFileName, "image/png", output.size(), attachmentComment);
-			attachment.setContent(page);
+			attachment = new Attachment(attachmentFileName, ATTACHMENT_MIMETYPE, output.size(), attachmentComment);
+			attachment.setContent(contentObject);
 
 			try {
-				this.attachmentManager.saveAttachment(attachment, null, attachmentData);
+				attachmentManager.saveAttachment(attachment, null, attachmentData);
 			} catch (IOException e) {
 				e.printStackTrace();
 				return null;
 			}
 		}
+		else
+		{
+			log.debug("Attachment was not null: {}", attachment.toString());
+		}
 
-		return (attachment == null) ? null :
-				"<div class=\"latex_img\"><img src=\"" + this.settingsManager.getGlobalSettings().getBaseUrl() +
-								attachment.getDownloadPath() + "\" /></div>";
+		String url = settingsManager.getGlobalSettings().getBaseUrl() + attachment.getDownloadPath();
+
+		log.debug("Attachment URL: {}", url);
+
+		return (attachment == null) ? null : "<div class=\"latex_img\"><img src=\"" + url + "\" /></div>";
+	}
+
+	private ContentEntityObject getPage(PageContext context, String pageTitleToRetrieve)
+	{
+		if (StringUtils.isBlank(pageTitleToRetrieve))
+			return context.getEntity();
+
+		String spaceKey = context.getSpaceKey();
+		String pageTitle = pageTitleToRetrieve;
+
+		int colonIndex = pageTitleToRetrieve.indexOf(":");
+		if (colonIndex != -1 && colonIndex != pageTitleToRetrieve.length() - 1)
+		{
+			spaceKey = pageTitleToRetrieve.substring(0, colonIndex);
+			pageTitle = pageTitleToRetrieve.substring(colonIndex + 1);
+		}
+
+		return pageManager.getPage(spaceKey, pageTitle);
 	}
 
 	@Override
